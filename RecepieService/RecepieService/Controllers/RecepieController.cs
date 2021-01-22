e123ed1aab8c4a77a8a0de756493b28790d80b8d
Luka Bobic <lukabobic728@gmail.com>
2021-01-22 22:26:40 +0100

    Final spring recepie

---
+++ RecepieService/RecepieService/Controllers/RecepieController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RecepieService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecepieService.Controllers
+{
+
+    [Microsoft.AspNetCore.Mvc.ApiController]
+    [Microsoft.AspNetCore.Components.Route("[controller]")]
+    public class RecepieController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Recepie> Get()
+        {
+            return Singlton.Instance.Recepies;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Recepie recepie)
+        {
+            Singlton.Instance.Recepies.Add(recepie);
+
+            return Ok();
+        }
+
+    }
+}
---
+++ RecepieService/RecepieService/Model/Singlton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecepieService.Model
+{
+    public class Singlton
+    {
+        private static Singlton instance;
+        private List<Recepie> recepies;
+        public Singlton()
+        {
+            recepies = new List<Recepie>();
+        }
+
+        public List<Recepie> Recepies { get; }
+
+        public static Singlton Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new Singlton();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
